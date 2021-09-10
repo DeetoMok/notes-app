85e809c28cc54ecab62729ab89d27634fb65ad1c
Mok Kai Sheng Daryl <54903425+DeetoMok@users.noreply.github.com>
2021-09-11 01:15:10 +0800

    finish project

--- mynotes/src/App.js
+++ mynotes/src/App.js
@@ -11,11 +11,13 @@ import NotePage from './pages/NotePage'
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Header />
-      {/* at home page, show NotesListPage component */}
-      <Route path='/' exact component={NotesListPage} />
-      <Route path='/note/:id' component={NotePage} />
+    <div className="container dark">
+      <div className="app">
+        <Header />
+        {/* at home page, show NotesListPage component */}
+        <Route path='/' exact component={NotesListPage} />
+        <Route path='/note/:id' component={NotePage} />
+      </div>
     </div>
     </Router>
   );
